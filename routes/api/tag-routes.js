5bb5d10ea33a6578ee058ccb7dfb0f1f480d272e
Jie Zou <jz3134@columbia.edu>
2022-11-04 23:22:12 -0700

    debug

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -10,7 +10,6 @@ router.get('/', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
       }
     ]
   })
@@ -31,7 +30,6 @@ router.get('/:id', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
       }
     ]
   })
